cd103d254c877b093990b6bd4b77fa9197c93ba3
vlada <2105>
2020-12-08 17:02:58 +0300

    Commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,10 +12,12 @@ public class Main {
                 new Line(-3, -8, -2.5),
                 new Line(1, -5, 0),
                 new HorizontalParabola(-1, -3, -0.24));
+        readCoordinate(picture);
+    }
 
-        Scanner scn = new Scanner(System.in);
-
+    private static void readCoordinate(Picture picture) {
         if (TestCase.testPicture(picture)) {
+            Scanner scn = new Scanner(System.in);
             System.out.println("Enter X cordinate: ");
             double x = scn.nextDouble();
             System.out.println("\nEnter Y cordinate: ");
